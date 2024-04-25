82d67ecac89bd4134431fec359b7d099252865f8
DeboraCapistana <160795576+DeboraCapistana@users.noreply.github.com>
2024-04-25 00:56:14 +0000

    aula06 praticas03

--- praticas/praticas1/questao02.c
+++ praticas/praticas1/questao02.c
@@ -1,10 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main (){
-  int base = 2;
-  int altura = 3;
-  int area = base * altura / 2;
-  printf("A area do triangulo é %i\n", area);
+int main () {
 
+  float base = 2.0;
+  float altura = 6.0; 
+  float area = 2.0;
+
+  printf("Entre com a base do triangulo: ");
+  scanf("&f", &base);
+
+  printf("Entre com a altura do triangulo");
+  scanf("&f", &altura);
+
+  printf("O valor da base é %f\n", base);")
+  scanf(base);
+  
+   float area = base * altura / 2;
+  printf ("A base do triangulo é: %f\n", area);
+  
+  
   return 0;
+
+  // fazer um programa em c que calcule a area de um triangulo (a=base x altura/2) obs: considerar a base e a altura inteiros maiores que 0//
+  
 }
